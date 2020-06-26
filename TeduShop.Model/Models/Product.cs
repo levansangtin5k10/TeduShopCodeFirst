4538e17a00a9722fab1acc1a34822785d1d2c26f
levansangtin5k10 <levansangtin5k10@gmail.com>
2020-06-27 01:48:39 +0700

    p

---
+++ TeduShop.Model/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using System.Xml.Linq;
+using TeduShop.Model.Abstract;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Products")]
+   public class Product:Auditable
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+
+        public string Alias { get; set; }
+        public int CategoryID { get; set; }
+        public string Image { get; set; }
+        public XElement MoreImages { get; set; }
+        public decimal Price { get; set; }
+        public decimal? PromotionPrice { get; set; }
+        public int? Warranty { get; set; }
+
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public bool? HomeFlag { get; set; }
+        public bool? HotFlag { get; set; }
+        public int? ViewCount { get; set; }
+
+        [ForeignKey("CategoryID")]
+        public virtual ProductCategory ProductCategory { get; set; }
+    }
+}
---
+++ TeduShop.Model/Models/ProductCategory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using TeduShop.Model.Abstract;
+
+namespace TeduShop.Model.Models
+{
+    [Table("ProductCategorys")]
+  public  class ProductCategory:Auditable
+    {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Alias { get; set; }
+        public int? ParentID { get; set; }
+        public int DisplayOrder { get; set; }
+
+        public string Image { get; set; }
+
+        public bool? HomeFlag { get; set; }
+
+        public virtual IEnumerable<Product> Products { get; set; }
+    }
+}
